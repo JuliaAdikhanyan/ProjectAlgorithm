ec051d130d05c58a25dbc010c8788218e8db6c57
Jules <jules.adikhanyan@gmail.com>
2020-07-01 20:29:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook("D:\\example.xlsx");
+
+
+
+    }
+}
